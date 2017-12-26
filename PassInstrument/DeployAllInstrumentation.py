94c9f9b1571f67330f7ca278cc89441cc3a81a61
JaredCJR <jaredcjr.tw@gmail.com>
2017-12-26 18:55:28 +0800

    Use bash to execute clang-tidy

--- PassInstrument/DeployAllInstrumentation.py
+++ PassInstrument/DeployAllInstrumentation.py
@@ -3,10 +3,12 @@ import os
 import sys
 import shutil
 import progressbar
+import subprocess
+import shlex
 
 class Instrumentation:
     # Realative path to 'llvm/lib/Transforms/'
-    InputList = [ \
+    InputList = [ 
             'Instrumentation/PGOMemOPSizeOpt.cpp',
             'InstCombine/InstructionCombining.cpp',
             'Utils/SimplifyInstructions.cpp',
@@ -42,16 +44,20 @@ class Instrumentation:
             'Scalar/MemCpyOptimizer.cpp',
             'Scalar/LowerExpectIntrinsic.cpp'
             ]
-    def AddBraces(self, InputFile, OutputFile, LLVMInclude):
-        os.system("clang-tidy -fix -config=\"{{Checks: \'readability-braces-around-statements\'}}\" {} -- -I{} -std=c++11 &> /dev/null".format(InputFile, LLVMInclude))
+    TestInputList = [ 
+            'Utils/LowerSwitch.cpp',
+            ]
+
+    def AddBraces(self, InputFile, OutputFile):
+        os.system("./script/runTidy.sh {}".format(InputFile))
         os.system("clang-format -style=LLVM {} > {}".format(InputFile, OutputFile))
 
-    def runRewriter(self, InputFile, OutputFile, LLVMInclude):
+    def runRewriter(self, InputFile, OutputFile, Headers):
         '''
         The -I/path/to/lib must comes from compiler
-        ex. $ gcc-7 -E -Wp,-v - < /dev/null
+        ex. $ clang -E -Wp,-v - < /dev/null
         '''
-        os.system("./PassRewriter {} -- -std=c++11 -I/usr/lib/gcc/x86_64-linux-gnu/7/include -I/usr/local/include -I/usr/lib/gcc/x86_64-linux-gnu/7/include-fixed -I/usr/include/x86_64-linux-gnu -I/usr/include -I{} > {}".format(InputFile, LLVMInclude, OutputFile))
+        os.system("./PassRewriter {} -- {} > {}".format(InputFile, Headers, OutputFile))
 
     def runAll(self):
         # Make sure the env exist.
@@ -62,6 +68,7 @@ class Instrumentation:
         OutputDir_ClangFormat = BaseDir + '/output-ClangFormat'
         OutputDir_Rewriter = BaseDir + '/output-Rewriter'
         LLVMInclude = LLVMsrc + '/include/llvm'
+        LLVMAdditional = LLVMsrc + '/lib/Transforms/InstCombine'
         # build dir tree
         if os.path.isdir(BaseDir):
             shutil.rmtree(BaseDir)
@@ -69,9 +76,12 @@ class Instrumentation:
         os.makedirs(InputDir)
         os.makedirs(OutputDir_ClangFormat)
         os.makedirs(OutputDir_Rewriter)
+        # headers for tidy and rewriter which use libtooling
+        Headers = "-std=c++11 -I/usr/local/include -I/usr/local/lib/clang/5.0.1/include -I/usr/include/x86_64-linux-gnu -I/usr/include -I{} -I{}".format(LLVMInclude, LLVMAdditional)
         # Time to rewrite them
         bar = progressbar.ProgressBar(max_value=len(self.InputList), redirect_stdout=True)
         i = 0
+        #for path in self.TestInputList:
         for path in self.InputList:
             # setup files related var
             AbsPath = LLVMsrc + '/lib/Transforms/' + path
@@ -85,11 +95,11 @@ class Instrumentation:
             # copy for rewriting
             os.system("cp {} {}".format(AbsPath, InputFile))
             # Make sure all braces are located properly
-            self.AddBraces(InputFile, OutputFile_ClangFormat, LLVMInclude)
+            self.AddBraces(InputFile, OutputFile_ClangFormat)
             # Rewrite it
-            self.runRewriter(OutputFile_ClangFormat, OutputFile_Rewriter, LLVMInclude)
+            self.runRewriter(OutputFile_ClangFormat, OutputFile_Rewriter, Headers)
             # Copy back for testing
-            os.system("cp {} {}".format(OutputFile_Rewriter, InputFile))
+            os.system("cp {} {}".format(OutputFile_Rewriter, AbsPath))
         print("All Done.")
             
 
---
+++ PassInstrument/script/runTidy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LLVMHeaders=${LLVM_THESIS_HOME}/include/llvm
+SystemHeaders=`cat ${LLVM_THESIS_HOME}/ThesisTools/PassInstrument/script/SystemHeaders`
+AdditionalHeaders=${LLVM_THESIS_HOME}/lib/Transforms/InstCombine
+
+Headers=$SystemHeaders" -I"$LLVMHeaders" -I"$AdditionalHeaders
+Input=$1
+
+clang-tidy -fix -config="{Checks: 'readability-braces-around-statements'}" $Input -- $Headers &> /dev/null
