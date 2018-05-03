4721b14ed2ff7e26af2020c477ead7b739366101
JaredCJR <jaredcjr.tw@gmail.com>
2018-05-03 22:50:10 +0800

    Fix sharedLib headers

--- PassInstrument/training/RewardPolicy2-Tools/sharedLib/headers/c/thesis_api.h
+++ PassInstrument/training/RewardPolicy2-Tools/sharedLib/headers/c/thesis_api.h
@@ -1,4 +1,5 @@
 #ifndef __THESIS_API_H
+#define __THESIS_API_H
 
 unsigned long long __thesis_getUserTime();
 void __thesis_LogTiming(unsigned long long entryTime, char *FuncName);
--- PassInstrument/training/RewardPolicy2-Tools/sharedLib/headers/cpp/thesis_api.h
+++ PassInstrument/training/RewardPolicy2-Tools/sharedLib/headers/cpp/thesis_api.h
@@ -1,4 +1,5 @@
 #ifndef __THESIS_API_H
+#define __THESIS_API_H
 
 extern "C" unsigned long long __thesis_getUserTime();
 extern "C" void __thesis_LogTiming(unsigned long long entryTime, char *FuncName);
