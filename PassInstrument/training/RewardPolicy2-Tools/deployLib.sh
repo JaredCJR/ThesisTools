fdf93f3f7e1c4aabc9c3218eb5f46f8e17554b11
JaredCJR <jaredcjr.tw@gmail.com>
2018-05-03 23:21:22 +0800

    Add library using script

---
+++ PassInstrument/training/RewardPolicy2-Tools/deployLib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd sharedLib
+# build .so
+make clean && make
+sudo mkdir /opt/lib
+sudo cp libfunctime.so.1.0 /opt/lib
+sudo ln -sf /opt/lib/libfunctime.so.1.0 /opt/lib/libfunctime.so.1
+sudo ln -sf /opt/lib/libfunctime.so.1 /opt/lib/libfunctime.so
+echo "Make sure the environment variables are proper set('C_INCLUDE_PATH', 'CPLUS_INCLUDE_PATH', 'LD_LIBRARY_PATH'and 'LIBRARY_PATH')."
