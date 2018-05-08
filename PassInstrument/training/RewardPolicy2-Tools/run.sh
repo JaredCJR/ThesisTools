026d1f0aad7f7f4eec8dde6e915d23a7b939370f
JaredCJR <jaredcjr.tw@gmail.com>
2018-05-08 20:52:25 +0800

    Add auto search for pass timing

--- PassInstrument/training/RewardPolicy2-Tools/run.sh
+++ PassInstrument/training/RewardPolicy2-Tools/run.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 LLVMHeaders=${LLVM_THESIS_HOME}/include/llvm
 SystemHeaders=`cat SystemHeaders`
-#AdditionalHeaders=${LLVM_THESIS_HOME}/tools/clang/lib/ARCMigrate
-Headers=$SystemHeaders" -I"$LLVMHeaders" -I"AdditionalHeaders
+Headers=$SystemHeaders" -I"$LLVMHeaders
 
-#build
-make clean
+#build rewriter
 make -j4
 
-input=test.c
-output=/tmp/$input
-cp $input $output
+# Make sure your test-suite is on "RewardPolicy2" branch
+TestSuiteBase=~/workspace/llvm-official-test/test-suite/
+Candidate_1=SingleSource/Benchmarks/Dhrystone/
+Candidate_2=SingleSource/Benchmarks/CoyoteBench
+Candidate_3=MultiSource/Applications/oggenc
+Candidate_4=MultiSource/Applications/sqlite3
+Candidate_5=MultiSource/Applications/aha
+#TODO: you have to assign the Candidate_x by yourself.
+# Target root dir(source dir of c/cpp files)
+input_dir=$TestSuiteBase$Candidate_5
+# Gather all source files
+C_SRC_LIST=`find $input_dir -type f -name '*.c' -printf '%p '`
+CXX_SRC_LIST=`find $input_dir -type f -name '*.cpp' -printf '%p '`
+# Get the header dir
+ProgHeaders_DIR=$input_dir
 
-#FIXME: cpp need "-std=c++11 " in $Headers
-$LLVM_THESIS_InstrumentHome/training/RewardPolicy2-Tools/RewriteWithTimeApi $output -- $Headers
+for src in $C_SRC_LIST" "$CXX_SRC_LIST
+do
+    # this will rewrite the original file
+    if [[ $src = *[!\ ]* ]] ; then
+        # "\$src contains characters other than space"
+        $LLVM_THESIS_InstrumentHome/training/RewardPolicy2-Tools/RewriteWithTimeApi $src -- $Headers" -I"$ProgHeaders_DIR
+        # insert header, make sure you already run deployLib.sh
+        sed -i '1i #include <thesis_api.h>' $src
+    fi
+done
 
-#TODO: cpp need --> extern "C"
-sed -i '1i #include <thesis_api.h>' $output
-
-echo "Rewritter to $output"
+echo "Rewritter done."
+echo "Make sure you know what this script has done."
+echo "Current rewriter for inseting APIs has bugs, and most of the bugs are for C++"
